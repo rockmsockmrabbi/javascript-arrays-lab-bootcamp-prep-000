0559eb238c28d3f834a4050cab2030a1d6d18049
Mark Shipley <mshipley1992@hotmail.com>
2018-08-09 20:53:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyRemoveFirstKitten(name) {
   return kittens.shift("Milo")
 }
 function appendKitten(name) {
-  return kittens.splice("Broom")
+  return kittens.splice(4,1,"Broom")
 }
