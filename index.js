7cd6923bda1f92e9472d44781d360aac1e60a87b
Mark Shipley <mshipley1992@hotmail.com>
2018-08-09 21:49:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function removeLastKitten(name) {
   return kittens.slice(0,2,"Garfield")
 }
 function removeFirstKitten(name) {
-  return kittens.slice([0])
+  return kittens.slice(-2,0,"Milo")
 }
